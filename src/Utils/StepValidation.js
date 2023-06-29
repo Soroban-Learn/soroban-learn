ab6d52c65c63d27bd9c5ed3d2a980b25d5815a84
John Dodds <john.dodds@populi.uk>
2023-06-29 21:22:59 +0100

    fix disable line bug

--- src/Utils/StepValidation.js
+++ src/Utils/StepValidation.js
@@ -64,6 +64,27 @@ export function useStepValidation(
         } else if (instruction.type === "code") {
           const ideCodeSingleLine = ideCode.replace(/\s+/g, "");
 
+          function getAllIndexes(arr, val) {
+            var indexes = [],
+              i = -1;
+            while ((i = arr.indexOf(val, i + 1)) != -1) {
+              indexes.push(i);
+            }
+            return indexes;
+          }
+
+          const allDoubleNewLineIndexes = getAllIndexes(ideCode, "\n\n");
+          const blockedRanges = [0];
+
+          allDoubleNewLineIndexes.forEach((index) => {
+            blockedRanges.push(index+1);
+            blockedRanges.push(index + 2);
+          });
+
+          blockedRanges.push(ideCode.length);
+          setBlockedRanges(blockedRanges);
+          console.log("[[[blockedRanges]]]", blockedRanges);
+
           if (ideCodeSingleLine === accumulatedInput + instruction.input) {
             setCurrentContentStep(currentStep + 1);
           } else {
@@ -72,6 +93,52 @@ export function useStepValidation(
         }
       });
     };
+
+    // const validateStep = () => {
+    //   instructions.forEach((instruction) => {
+    //     if (instruction.type === "terminal") {
+    //       if (instruction.input === consoleInput) {
+    //         setCurrentContentStep(currentStep + 1);
+    //       } else {
+    //         setHasError(true);
+    //         setCurrentError("Invalid Command");
+    //       }
+    //     } else if (instruction.type === "code") {
+    //       let ideCodeSingleLine = ideCode.replace(/\s+/g, "");
+    //       const charPosOfAllDoubleNewLines = [];
+    //       let charPosOfDoubleNewLine = ideCodeSingleLine.indexOf("\n\n");
+
+    //       function getAllIndexes(arr, val) {
+    //         var indexes = [], i = -1;
+    //         while ((i = arr.indexOf(val, i+1)) != -1){
+    //             indexes.push(i);
+    //         }
+    //         return indexes;
+    //       }
+
+    //       const allDoubleNewLineIndexes = getAllIndexes(ideCode, "\n\n");
+    //       const blockedRanges = [0];
+
+    //       allDoubleNewLineIndexes.forEach((index) => {
+    //         blockedRanges.push(index);
+    //         blockedRanges.push(index + 2);
+    //       });
+
+    //     blockedRanges.push(ideCode.length);
+    //     setBlockedRanges(blockedRanges);
+    //     console.log("[[[blockedRanges]]]", blockedRanges);
+
+    //     console.log("[[[ideCodeSingleLine]]]", ideCodeSingleLine);
+    //       console.log("[[[accumulatedInput]]]", accumulatedInput);
+
+    //       if (ideCodeSingleLine === accumulatedInput) {
+    //         setCurrentContentStep(currentStep + 1);
+    //       } else {
+    //         setError("Invalid Code");
+    //       }
+    //     }
+    //   });
+    // };
 
     return { validateStep };
   }
